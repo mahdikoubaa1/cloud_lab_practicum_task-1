e6c5c156271c3ab05f0ac68cdbaf4f097c9d7c79
mahdikoubaa1 <65303454+mahdikoubaa1@users.noreply.github.com>
2023-10-28 23:02:41 +0200

    Delete build directory

--- build/CMakeFiles/kvs.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for kvs.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mahdi/Documents/Task1-mahdikoubaa1
-# Build directory: /home/mahdi/Documents/Task1-mahdikoubaa1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
